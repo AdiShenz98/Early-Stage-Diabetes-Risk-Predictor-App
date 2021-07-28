01f9863a3dca76f5a12f851cf442cd9f0d1a10e5
Aditya Shenoy <49498445+AdiShenz98@users.noreply.github.com>
2021-07-28 21:38:24 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,6 @@
 mkdir -p ~/.streamlit/
 
-echo "[theme]
-primaryColor = ‘#0a1931’
-backgroundColor = ‘#f1ecc3’
-secondaryBackgroundColor = ‘#3f5669’
-textColor= ‘#fe7171’
-font = ‘sans serif’
-[server]
+echo "[server]
 headless = true
 port = $PORT
 enableCORS = false
